44c9af942ba74e64659e7af2f1464706ecd14aae
bur10364 <robert.buettner@stud.uni-regensburg.de>
2026-09-25 23:23:48 -0600

    Added Startercode for Ex-3

---
+++ src/main/java/FunctionalErrorHandling/Main.java
@@ -0,0 +1,23 @@
+package FunctionalErrorHandling;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Map<Integer, String> movies = Map.of(
+                2000, "Mission: Impossible II",
+                2002, "The Lord of the Rings: The Two Towers",
+                2004, "Shrek 2",
+                2006, "Pirates of the Caribbean: Dead Man's Chest",
+                2009, "Avatar",
+                2011, "Harry Potter and the Deathly Hallows – Part 2",
+                2012, "The Avengers",
+                2015, "Star Wars: The Force Awakens",
+                2019, "Avengers: Endgame",
+                2022, "Avatar: The Way of Water"
+        );
+
+    }
+}
